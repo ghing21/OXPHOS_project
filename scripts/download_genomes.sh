bbad8c29a2a07ce697aed33bc43d9d9c6cf25f4e
ghing21 <bhavyagudhka2001@gmail.com>
2026-02-23 11:39:59 +0100

    Ignore GenBank genome files

--- scripts/download_genomes.sh
+++ scripts/download_genomes.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Script to generate accession.txt and download mitochondrial genomes
+# Script to generate accession.txt and download mitochondrial genomes anf genbank files
+
 
 # Step 1: Remove carriage returns from accession.txt (if any)
 sed -i 's/\r$//' config/accession.txt
@@ -10,4 +11,10 @@ mkdir -p data/genomes
 # Step 3: Download genomes from NCBI
 while read acc; do
   efetch -db nucleotide -id "$acc" -format fasta > data/genomes/${acc}.fasta
+done < config/accession.txt
+
+# Step 4: Download genbank files
+while read acc; do
+    echo "Downloading $acc ..."
+    efetch -db nucleotide -id "$acc" -format gb > data/genomes_gb/${acc}.gb
 done < config/accession.txt
